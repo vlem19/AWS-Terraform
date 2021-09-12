4c9b619f21499bcbb8c47a9a99193a7aacd4f3b5
vlem19 <73515997+vlem19@users.noreply.github.com>
2021-09-13 06:12:27 +0700

    Add files via upload

--- vpc.tf
+++ vpc.tf
@@ -1,25 +1,8 @@
-locals {
+resource "aws_vpc" "ddbtable" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-  attributes = concat(
-    [
-      {
-        name = var.range_key
-        type = var.range_key_type
-      },
-      {
-        name = var.hash_key
-        type = var.hash_key_type
-      }
-    ],
-    var.dynamodb_attributes
-  )
-}
-
-resource "aws_dynamodb_table" "ddbtable" {
-  cidr_block = "10.0.0.0/16"
-  name           = var.table_name
-  hash_key       = var.hash_key
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 2
-  write_capacity = 2
+  tags = {
+    Name = "ddbtable"
+  }
 }
